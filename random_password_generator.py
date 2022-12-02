0b055bd7e0b47447178f4a32749d5d94a1fa5f07
marta-rakowska <113170762+marta-rakowska@users.noreply.github.com>
2022-12-02 17:49:13 +0100

    Add files via upload
    Add comments.
    Generate random characters.
    Generate password.

--- random_password_generator.py
+++ random_password_generator.py
@@ -6,22 +6,23 @@ import random
 
 #A function do shuffle all the characters of a string
 def shuffle(string):
-  tempList = list(string)
-  random.shuffle(tempList)
-  return ''.join(tempList)
+  temp_list = list(string)
+  random.shuffle(temp_list)
+  return ''.join(temp_list)
 
 #Main program starts here
 uppercase_letter_1=chr(random.randint(65,90)) #Generate a random Uppercase letter (based on ASCII code)
 uppercase_letter_2=chr(random.randint(65,90)) #Generate a random Uppercase letter (based on ASCII code)
-lowercase_letter_1 =
-lowercase_letter_2 =
-digit_1
-digit_2
-punctuation_sign_1
-punctuation_sign_2
+lowercase_letter_1 = chr(random.randint(97,122)) #Generate a random lowercase letter (based on ASCII code)
+lowercase_letter_2 = chr(random.randint(97,122)) #Generate a random lowercase letter (based on ASCII code)
+digit_1 = chr(random.randint(48,57)) #Generate a random digit (based on ASCII code)
+digit_2 = chr(random.randint(48,57)) #Generate a random digit (based on ASCII code)
+punctuation_sign_1 = chr(random.randint(33,47)) #Generate a random punctuation sign (based on ASCII code)
+punctuation_sign_2 = chr(random.randint(58,64)) #Generate a random punctuation sign (based on ASCII code)
 
 #Generate password using all the characters, in random order
-password = uppercaseLetter1 + uppercaseLetter2
+password = uppercase_letter_1 + uppercase_letter_2 + lowercase_letter_1 + lowercase_letter_2 + digit_1 + digit_2\
+           + punctuation_sign_1 + punctuation_sign_2
 password = shuffle(password)
 
 #Ouput
